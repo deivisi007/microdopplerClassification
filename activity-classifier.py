b9bc2c7615d3ad8bfd3c388be01e6f0759205600
deivisi007 <deivisi007@hotmail.com>
2023-03-05 22:07:01 -0500

    Add files via upload

---
+++ activity-classifier.py
@@ -0,0 +1,290 @@
+import pandas as pd
+import numpy as np 
+import itertools
+import keras
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img 
+from keras.models import Sequential 
+from keras import optimizers
+from keras.preprocessing import image
+from keras.layers import Dropout, Flatten, Dense  
+from keras import applications  
+from keras.utils.np_utils import to_categorical  
+import matplotlib.pyplot as plt 
+import matplotlib.image as mpimg
+%matplotlib inline
+import math  
+import datetime
+import time
+import os
+import tensorflow as tf
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+
+gpu_options = tf.compat.v1.GPUOptions(allow_growth=True)
+sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options))
+
+img_width, img_height = 224, 224  
+   
+top_model_weights_path = 'bottleneck_fc_model.h5' 
+
+train_data_dir = 'C:/Users/devi/Desktop/microdoppler-classifications/data/train'  
+validation_data_dir = 'C:/Users/devi/Desktop/microdoppler-classifications/data/validation'  
+test_data_dir = 'C:/Users/devi/Desktop/microdoppler-classifications/data/test'
+   
+ 
+epochs = 7    
+batch_size = 50  
+
+#Loading vgc16 model
+vgg16 = applications.VGG16(include_top=False, weights='imagenet') 
+
+datagen = ImageDataGenerator(rescale=1. / 255) 
+
+start = datetime.datetime.now()
+   
+generator = datagen.flow_from_directory(  
+     train_data_dir,  
+     target_size=(img_width, img_height),  
+     batch_size=batch_size,  
+     class_mode=None,  
+     shuffle=False)  
+   
+nb_train_samples = len(generator.filenames)  
+num_classes = len(generator.class_indices)  
+   
+predict_size_train = int(math.ceil(nb_train_samples / batch_size))  
+   
+bottleneck_features_train = vgg16.predict_generator(generator, predict_size_train)  
+   
+np.save('bottleneck_features_train.npy', bottleneck_features_train)
+end= datetime.datetime.now()
+elapsed= end-start
+print ('Time: ', elapsed)
+
+print('-'*117)
+
+start = datetime.datetime.now()
+generator = datagen.flow_from_directory(  
+     validation_data_dir,  
+     target_size=(img_width, img_height),  
+     batch_size=batch_size,  
+     class_mode=None,  
+     shuffle=False)  
+   
+nb_validation_samples = len(generator.filenames)  
+   
+predict_size_validation = int(math.ceil(nb_validation_samples / batch_size))  
+   
+bottleneck_features_validation = vgg16.predict_generator(  
+     generator, predict_size_validation)  
+   
+np.save('bottleneck_features_validation.npy', bottleneck_features_validation) 
+end= datetime.datetime.now()
+elapsed= end-start
+print ('Time: ', elapsed)
+
+print('-'*117)
+
+start = datetime.datetime.now()
+generator = datagen.flow_from_directory(  
+     test_data_dir,  
+     target_size=(img_width, img_height),  
+     batch_size=batch_size,  
+     class_mode=None,  
+     shuffle=False)  
+   
+nb_test_samples = len(generator.filenames)  
+   
+predict_size_test = int(math.ceil(nb_test_samples / batch_size))  
+   
+bottleneck_features_test = vgg16.predict_generator(  
+     generator, predict_size_test)  
+   
+np.save('bottleneck_features_test.npy', bottleneck_features_test) 
+end= datetime.datetime.now()
+elapsed= end-start
+print ('Time: ', elapsed)
+
+#training data
+generator_top = datagen.flow_from_directory(  
+         train_data_dir,  
+         target_size=(img_width, img_height),  
+         batch_size=batch_size,  
+         class_mode='categorical',  
+         shuffle=False)  
+   
+nb_train_samples = len(generator_top.filenames)  
+num_classes = len(generator_top.class_indices)  
+   
+train_data = np.load('bottleneck_features_train.npy')  
+
+train_labels = generator_top.classes  
+
+train_labels = to_categorical(train_labels, num_classes=num_classes) 
+
+#validation data
+generator_top = datagen.flow_from_directory(  
+         validation_data_dir,  
+         target_size=(img_width, img_height),  
+         batch_size=batch_size,  
+         class_mode=None,  
+         shuffle=False)  
+   
+nb_validation_samples = len(generator_top.filenames)  
+   
+validation_data = np.load('bottleneck_features_validation.npy')  
+   
+
+validation_labels = generator_top.classes  
+validation_labels = to_categorical(validation_labels, num_classes=num_classes)  
+
+generator_top = datagen.flow_from_directory(  
+         test_data_dir,  
+         target_size=(img_width, img_height),  
+         batch_size=batch_size,  
+         class_mode=None,  
+         shuffle=False)  
+   
+nb_test_samples = len(generator_top.filenames)  
+   
+test_data = np.load('bottleneck_features_test.npy')  
+   
+test_labels = generator_top.classes  
+test_labels = to_categorical(test_labels, num_classes=num_classes)
+
+start = datetime.datetime.now()
+model = Sequential()  
+model.add(Flatten(input_shape=train_data.shape[1:]))  
+model.add(Dense(100, activation=keras.layers.LeakyReLU(alpha=0.3)))  
+model.add(Dropout(0.5))  
+model.add(Dense(50, activation=keras.layers.LeakyReLU(alpha=0.3)))  
+model.add(Dropout(0.3)) 
+model.add(Dense(num_classes, activation='softmax'))  
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])  
+
+history = model.fit(train_data, train_labels,  
+      epochs=7,
+      batch_size=batch_size,  
+      validation_data=(validation_data, validation_labels))  
+
+model.save_weights(top_model_weights_path)  
+
+(eval_loss, eval_accuracy) = model.evaluate(  
+ validation_data, validation_labels, batch_size=batch_size, verbose=1)
+
+print("[INFO] accuracy: {:.2f}%".format(eval_accuracy * 100))  
+print("[INFO] Loss: {}".format(eval_loss))  
+end= datetime.datetime.now()
+elapsed= end-start
+print ('Time: ', elapsed)
+
+
+model.summary()
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(len(acc))
+plt.plot(epochs, acc, 'r', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.ylabel('accuracy')  
+plt.xlabel('epoch')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.ylabel('loss')  
+plt.xlabel('epoch')
+plt.legend()
+plt.show()
+
+model.evaluate(test_data, test_labels)
+
+print('test data', test_data)
+preds = np.round(model.predict(test_data),0) 
+
+print('rounded test_labels', preds)
+
+activities = ['activity1', 'activity2', 'activity3', 'activity4', 'activity5']
+classification_metrics = metrics.classification_report(test_labels, preds, target_names=activities )
+print(classification_metrics)
+
+categorical_test_labels = pd.DataFrame(test_labels).idxmax(axis=1)
+categorical_preds = pd.DataFrame(preds).idxmax(axis=1)
+
+confusion_matrix= confusion_matrix(categorical_test_labels, categorical_preds)
+
+def plot_confusion_matrix(cm, classes,
+             normalize=False,
+             title='Confusion matrix',
+             cmap=plt.cm.Blues):
+    '''prints pretty confusion metric with normalization option '''
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+    
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+plot_confusion_matrix(confusion_matrix, ['activity1', 'activity2', 'activity3', 'activity4', 'activity5'])
+
+plot_confusion_matrix(confusion_matrix, 
+                      ['activity1', 'activity2', 'activity3', 'activity4', 'activity5'],
+                     normalize=True)
+
+def read_image(file_path):
+    print("[INFO] loading and preprocessing image...")  
+    image = load_img(file_path, target_size=(224, 224))  
+    image = img_to_array(image)     
+    image = np.expand_dims(image, axis=0)
+    image /= 255.  
+    return image
+
+
+def test_single_image(path):
+    activities = ['activity1', 'activity2', 'activity3', 'activity4', 'activity5']
+    images = read_image(path)
+    time.sleep(.5)
+    bt_prediction = vgg16.predict(images)  
+    preds = model.predict_proba(bt_prediction)
+    for idx, activities, x in zip(range(0,6), activities , preds[0]):
+        print("ID: {}, Label: {} {}%".format(idx, activities, round(x*100,2) ))
+    print('Final Decision:')
+    time.sleep(.5)
+    for x in range(3):
+        print('.'*(x+1))
+        time.sleep(.2)
+    class_predicted = model.predict_classes(bt_prediction)
+    class_dictionary = generator_top.class_indices  
+    inv_map = {v: k for k, v in class_dictionary.items()}  
+    print("ID: {}, Label: {}".format(class_predicted[0], inv_map[class_predicted[0]]))  
+    return load_img(path)
+
+path = 'data/test/activity222.jpg'
+
+test_single_image(path)
